5412bfcbdd65a0a68d0461b85f555925e1c506e2
Evgeny Belkin <sunafajro@gmail.com>
2019-11-17 22:26:47 +0300

    Псевдонимы для папок с контентом

--- config/aliases.php
+++ config/aliases.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    '@config' => dirname(__DIR__) . '/config/local',
-    '@data'   => dirname(__DIR__) . '/data',
+    '@config'  => dirname(__DIR__) . '/config/local',
+    '@data'    => dirname(__DIR__) . '/data',
+    '@content' => dirname(__DIR__) . '/data/content',
 ];
